dd75a518d057dcb1dc8f147ca534785bd8af9c96
Kevin Bond <kevinbond@gmail.com>
2011-04-13 12:45:24 -0400

    added resources

--- server-setup.sh
+++ server-setup.sh
@@ -1,22 +1,51 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
+EXPECTED_ARGS=2
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` {name} {email}"
+  exit
+fi
+
 echo "Installing git/subversion"
-apt-get install git subversion
+sudo apt-get install git subversion
+
+echo "Configurating git"
+sudo git config --global user.name "$1"
+sudo git config --global user.email "$2"
 
 echo "Installing Apache"
-apt-get install apache2 apache2-utils
-a2enmod rewrite vhost_alias
+sudo apt-get install apache2 apache2-utils
+sudo a2enmod rewrite vhost_alias
 
 echo "Installing PHP"
-apt-get install php5 libapache2-mod-php5 php5-cli php5-xdebug php-pear
+sudo apt-get install php5 libapache2-mod-php5 php5-cli php5-xdebug php-pear
 
 echo "Restarting Apache"
-/etc/init.d/apache2 restart
+sudo apache2ctl restart
 
 echo "Installing MySQL"
-apt-get install mysql-server
-apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin
+sudo apt-get install mysql-server
+sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin
+
+echo "Setup MySqlAdmin"
+sudo cp "$BASEDIR/resources/phpmyadmin.config.inc.php" /etc/phpmyadmin/config.inc.php
 
 echo "Restarting Apache"
-/etc/init.d/apache2 restart
+sudo apache2ctl restart
+
+echo "Setup www dir"
+sudo cp "$BASEDIR/resources/dev" /etc/apache2/sites-available/
+sudo a2dissite 000-default
+sudo a2ensite dev
+sudo apache2ctl restart
+
+if [ ! -e "$HOME/.ssh/id_rsa.pub" ]; then
+  echo "Generate SSH Key"
+  ssh-keygen -t rsa -C "$2"  
+fi
+echo "SSH Key:"
+cat ~/.ssh/id_rsa.pub
 
